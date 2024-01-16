5d68f282a7179ea2ea69b5facc85a4f1944b3dd1
Achal <achal@achal.me>
2024-01-16 11:08:31 -0800

    New animation & update help guide link (#9)
    * New animation & update help guide link
    
    * Tweak og-image

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,18 @@ import { fontCalibre } from '@/fonts/fonts';
 export const metadata: Metadata = {
   title: 'Flashbots Protect',
   description: 'A safer way to transact on Ethereum',
+  openGraph: {
+    type: 'website',
+    url: 'https://protect.flashbots.net',
+    title: 'Flashbots Protect',
+    description: 'A safer way to transact on Ethereum, by Flashbots.',
+    siteName: 'Protect',
+    images: [
+      {
+        url: '/og-image.png',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
