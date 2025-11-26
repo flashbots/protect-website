883c8654512be6d1b020af9a820792c28e4b9629
Ilya Lukyanov <ilya@luk.moe>
2025-11-26 13:45:25 +0000

    Add Onion-Location header on /
    - Add Onion-Location header to root path for Tor Browser displaying
      neat button

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   async headers() {
     return [
       {
+        // show redirect button in Tor Browser
+        source: '/',
+        headers: [
+          {
+            key: 'Onion-Location',
+            value: 'http://protectfbnoqyfgo3t5ouw3c7odp55qqoxnfdd7u24nzz5pkbclbzzyd.onion',
+          },
+        ],
+      },
+      {
         // allow iframe parent to access static files for Protect Button
         source: '/_next/static/:path*',
         headers: [
